0770bd6978971e94369b1b3ad7469754afcd4581
Viratsinha <49249920+Viratsinha@users.noreply.github.com>
2019-04-05 00:20:01 +0530

    Update JobDetails.java

--- JobDetails.java
+++ JobDetails.java
@@ -1,55 +1,34 @@
-/*
-#include<stdio.h>
 
-struct jobDetails{
-	char processName[20];
-	int processId;
-	int arrivalTime;
-	int burstTime;	
-};
-
-void main(){
-	int total_query, map;
-	struct jodDetails student[100], faculty[100];
-	printf("Welcome, Mr. Sudhesh\n");
-	printf("Enter the total number of process to handle: ");
-	scanf("%d", &total_query);
-	for(int i=0; i<total_query; i++){
-		printf("Query Type (1 for faculty || 2 for Student): ");
-		scanf("%d", &map);
-		if(map==1){
-
-		} 
-		else{
-
-		}
-	}
-}
-*/
 import java.util.Scanner;
 import java.lang.*;
 import java.lang.Math;
 
 class JobDetails{
-	char processName[20];
+	char processName[]=new char[20];
+	
 	int processId;
 	int arrivalTime;
 	int burstTime;
-char student[100], faculty[100];	
+char student[]= new char[100]; 
+char faculty[]= new char[100];	
 	
-}
+
 public static void main(String[]args)
 {
 	int total_query, map;
 	System.out.println("Welcome, Mr. Sudhesh\n");
 	System.out.println("Enter the total number of process to handle: ");
 	Scanner s = new Scanner(System.in);
-	s.total_query;
+	total_query= s.nextInt();
 	
 	for(int i=0; i<total_query; i++){
 		System.out.println("Query Type (1 for faculty and 2 for Student): ");
 		Scanner s1 = new Scanner(System.in);
-		s1.map;
+		map = s1.nextInt();
 		if(map==1){
 
-		} 
+		}
+
+	}
+}	
+}
