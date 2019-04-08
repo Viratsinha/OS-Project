a4698604a8e44ad5ed11b7ea894ac0834de7ccc7
Viratsinha <49249920+Viratsinha@users.noreply.github.com>
2019-04-08 19:39:47 +0530

    changes in main function

--- osprogram.c
+++ osprogram.c
@@ -3,18 +3,19 @@
 
 int main()
 {
+ 
  while(1){
 	int details;
-  printf("\n Choose what you are : \n1.Faculty \t2.Student \t3.Exit\n");
+  printf("\n Choose what you are : \n1.press 1 for Faculty \t2.press 2 for Student \t3.press 3 for Exit\n");
   scanf("%d",&details);
   switch(details){
   	case 1:
   		printf("\n\t\t For Faculty Queries: ");
-  		Roundrobin();
+  		
   		break;
   	case 2:
   		printf("\n\t\t For Student Queries:");
-  		Roundrobin();
+  		
   		break;
   	case 3:
   		printf("\nProgram has been terminated ");
@@ -26,3 +27,5 @@ int main()
   }
 }
 }
+
+
